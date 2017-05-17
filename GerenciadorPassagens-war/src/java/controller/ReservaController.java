786c609e441edf9b0a964db1c910c166f0917708
09123471980 <09123471980@ufsc240846.ufsc.br>
2017-05-17 20:05:44 -0300

    reservas e tretas resolvidas

---
+++ GerenciadorPassagens-war/src/java/controller/ReservaController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import entities.Reserva;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import model.ReservaFacade;
+
+/**
+ *
+ * @author 09123471980
+ */
+@Named(value = "reservaController")
+@SessionScoped
+public class ReservaController implements Serializable {
+
+    @EJB
+    private ReservaFacade reservaFacade;
+    private Reserva reserva = new Reserva();
+
+   
+    
+    public ReservaController() {
+    }
+    
+    public List<Reserva> findAll(){
+        return this.reservaFacade.findAll();
+    }
+    
+    public void insert(int idCliente, int idPassagem){
+        
+    }
+    
+    public String insert(){
+        try{
+            this.reservaFacade.create(reserva);
+        }catch(Exception e){
+            System.err.println(e);
+        }
+        this.reserva = new Reserva();
+        return "listaPassagens";
+    }
+    
+     public Reserva getReserva() {
+        return reserva;
+    }
+
+    public void setReserva(Reserva reserva) {
+        this.reserva = reserva;
+    }
+    
+    
+}
